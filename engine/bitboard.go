462eb3c235b4c1030e3a21cdc938e40b9ed20f6d
Peter Stace <peterstace@gmail.com>
2022-05-02 06:02:06 +1000

    Add missing parts of position state

--- engine/bitboard.go
+++ engine/bitboard.go
@@ -1,5 +1,7 @@
 package engine
 
+import "fmt"
+
 /*
  The index in each square is the position in a uint64 (with 0 being the LSB and
  63 being the MSB):
@@ -55,6 +57,14 @@ const (
 	fileCF = fileC | fileF
 )
 
+func rank(i int) uint64 {
+	return 0xff << (8 * i)
+}
+
+func file(i int) uint64 {
+	return 0x0101010101010101 << i
+}
+
 type side byte
 
 const (
@@ -63,22 +73,64 @@ const (
 )
 
 type Position struct {
+	// side bitboards indicate the colour of a piece occupying a square.
 	side [2]uint64
+
+	// piece specific bitboards show if a square is occupied by a piece.
 	pawn uint64
 	nite uint64
 	bish uint64
 	rook uint64
 	quee uint64
 	king uint64
-	// TODO: half-move clock for the 50 move rule
-	// TODO: castling rights
-	// TODO: En passant
-	// TODO: side to move
+
+	// hmc (half move clock) counts the number of (half) moves since a pawn has
+	// been captured captured or moved.
+	hmc uint16
+
+	// ep show en passant rights for each file.
+	ep [2]uint8
+
+	// flags stores flags specific to each side in each nibble. See functions
+	// named `*Mask` for details.
+	flags uint8
+
 	// TODO: Also have to manage move list somehow for repetition.
 }
 
+func queenCastleMask(s side) uint8 {
+	return 0x01 << (s * 4)
+}
+
+func kingCastleMask(s side) uint8 {
+	return 0x02 << (s * 4)
+}
+
+func sideToMoveMask(s side) uint8 {
+	return 0x04 << (s * 4)
+}
+
+func (p *Position) assertInvariants() {
+	if conj := (p.side[0] & p.side[1]); conj != 0 {
+		panic(fmt.Sprintf(
+			"non-zero side conjunction: %d & %d: %d",
+			p.side[0], p.side[1], conj,
+		))
+	}
+	// TODO: additional assertions
+}
+
+//func (p *Position) FEN() string {
+//	for ri := 7; ri >= 0; ri-- {
+//		r := rank(ri)
+//		for fi := 0; fi < 8; fi++ {
+//			sq := r & file(fi)
+//		}
+//	}
+//}
+
 func InitialPosition() Position {
-	return Position{
+	pos := Position{
 		side: [2]uint64{rank12, rank78},
 		pawn: rank27,
 		nite: rank18 & fileBG,
@@ -87,4 +139,6 @@ func InitialPosition() Position {
 		quee: rank18 & fileD,
 		king: rank18 & fileE,
 	}
+	pos.assertInvariants()
+	return pos
 }
---
+++ engine/position_test.go
@@ -0,0 +1,11 @@
+package engine_test
+
+import (
+	"testing"
+
+	"github.com/peterstace/starquake/engine"
+)
+
+func TestInitialPosition(t *testing.T) {
+	engine.InitialPosition()
+}
